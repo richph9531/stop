1aedc62b41e71675af0f377ad1438a42d1f9e993
Richard Philips <richard.philips@johnlewis.co.uk>
2025-02-27 20:12:36 +0200

    try again

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   use: {
     baseURL: process.env.CI 
-      ? `http://localhost:3000${process.env.NEXT_PUBLIC_BASE_PATH || ''}`
+      ? 'http://localhost:3000'
       : 'http://localhost:3000',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
