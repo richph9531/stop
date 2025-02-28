f6316e6a232da8e516a15169b38083308446b01d
Richard Philips <richard.philips@johnlewis.co.uk>
2025-02-28 07:36:02 +0200

    retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,14 +4,14 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  // retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 0,
   workers: 5,
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    deviceScaleFactor: 1, // Force 1:1 pixel ratio
-    // viewport: { width: 1280, height: 720 }, // Set consistent viewport
+    deviceScaleFactor: 1,
+
   },
   projects: [
     {
